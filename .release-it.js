11526c5d3c006e78f57eb9fa0dda609b26feb299
Lasim <hajdas@hajdas.de>
2025-03-31 21:42:05 +0200

    chore(release): enhance release-it configuration with writer options

--- .release-it.js
+++ .release-it.js
@@ -34,7 +34,14 @@ module.exports = {
         {"type": "refactor", "section": "Code Refactoring"},
         {"type": "perf", "section": "Performance Improvements"},
         {"type": "test", "section": "Tests"}
-      ]
+      ],
+      "writerOpts": {
+        "commitsSort": ["subject", "scope"],
+        "includeDetails": true,
+        "ignoreReverted": true,
+        "issuePrefixes": ["#"],
+        "filterByAuthor": false
+      }
     }
   }
 };
